276b9802a8fb49ea6232321461ee541a947e6c9d
saifuddin24 <saif.bigc@gmail.com>
2020-12-11 06:28:48 +0600

    Multiple pages created

---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,4 @@
+
+export default () => {
+    return <div>For your password?</div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import Homepage from "./Homepage";
+import About from "./About";
+import Contact from "./Contact";
+import ForgotPassword from "./ForgotPassword";
+import Login from "./Login";
+import Profile from "./Profile";
+import QuizList from "./QuizList";
+import Registration from "./Registration";
+import ResetPassword from "./ResetPassword";
+import Subjects from "./Subjects";
+import Settings from "./Settings";
 
-export { Homepage };
+export { Homepage, About, Contact, ForgotPassword, Login, Profile, QuizList, Registration, ResetPassword, Subjects, Settings };
